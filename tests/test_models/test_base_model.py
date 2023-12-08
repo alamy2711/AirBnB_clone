7249f8f8452137b39a0ad9bd27129e25b8d4ddc0
Omar-Driouch <omardriouch29@gmail.com>
2023-12-08 15:21:51 +0100

    add test

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+"""
+
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+class Test_Base_Model_init(unittest.TestCase):
+    """
+    """
+    
+    def test__init(self):
+        my_model = BaseModel()
+
+        self.assertEqual(str, type(BaseModel().id))
+        self.assertEqual(datetime, type(BaseModel().created_at))
+        self.assertEqual(datetime, type(BaseModel().updated_at))
+
+    def test__save(self):
+        my_model = BaseModel()
+
+        initial_updated_at = my_model.updated_at
+        my_model.save()
+        current_updated_at = my_model.updated_at
+        
+        self.assertNotEqual(initial_updated_at, current_updated_at)
+    
+    def test_to_dic(self):
+        my_model = BaseModel()
+
+        model_dic = my_model.to_dict()
+        self.assertIsInstance(model_dic, dict)
+
+        self.assertEqual(model_dic["__class__"], my_model.__class__.__name__)
+        self.assertEqual(model_dic["id"], my_model.id)
+        self.assertEqual(model_dic["created_at"], my_model.created_at)
+        self.assertEqual(model_dic["updated_at"], my_model.updated_at)
+
+
+    def test_str_method(self):
+        my_model = BaseModel()
+
+        self.assertTrue(str(my_model).startswith('[BaseModel]'))
+        self.assertIn(my_model.id, str(my_model))
+        self.assertIn(str(my_model.__dict__),str(my_model))
+
+if __name__ == "__main__":
+    unittest.main()
