41a7a5db8d6699fa670502da2741a49c3b0153b4
Alamy <mestafaalami11@gmail.com>
2023-12-08 15:16:37 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,14 @@
 
 import uuid
 import datetime
+from . import storage
 
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -15,18 +19,17 @@ class BaseModel:
                 elif key != "__class__":
                     setattr(self, key, value)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_dict(self):
-        dict_result = self.__dict__
+        dict_result = self.__dict__.copy()
         dict_result.update(
             {
                 "__class__": __class__.__name__,
