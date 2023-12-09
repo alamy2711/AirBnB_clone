13262754ac717da17851dcc9ea22824c8dcf2350
Alamy <mestafaalami11@gmail.com>
2023-12-09 21:44:25 +0100

    Add review.py

---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Review  Module defines the Review  class"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Review class representing a review."""
+
+    place_id = ""
+    user_id = ""
+    text = ""
