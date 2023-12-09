067ed1ca53f04b2032e81bf0e949b269a6101db1
Alamy <mestafaalami11@gmail.com>
2023-12-09 21:32:10 +0100

    Add city.py

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""City Module defines the City class"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """City class representing a city."""
+
+    state_id = ""
+    name = ""
