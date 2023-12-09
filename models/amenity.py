27482fbb54b44277a25a9d17f8126d188f0cbbe9
Alamy <mestafaalami11@gmail.com>
2023-12-09 21:35:49 +0100

    Add amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Amenity  Module defines the Amenity  class"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity  class representing an amenity."""
+
+    name = ""
