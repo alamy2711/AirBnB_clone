48ee0a8aec57c40e9701879b71de76f68085f7f8
Alamy <mestafaalami11@gmail.com>
2023-12-09 21:48:23 +0100

    Update console.py

--- console.py
+++ console.py
@@ -5,6 +5,11 @@ import shlex
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
