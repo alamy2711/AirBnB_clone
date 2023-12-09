f842d049ea6e4dc4595520966fc9771d9789032a
Alamy <mestafaalami11@gmail.com>
2023-12-09 20:56:28 +0100

    Add user.py

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""User Module defines the User class"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User class representing user data."""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
