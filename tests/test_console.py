bd5ca2747ca44c2cd5be77ce982181922a0bf9ac
Alamy <mestafaalami11@gmail.com>
2023-12-10 14:34:05 +0100

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -13,18 +13,18 @@ from models.engine.file_storage import FileStorage
 from console import HBNBCommand
 
 
-
 class TestHBNBCommand_prompting(unittest.TestCase):
     """Unittests for testing prompting of the HBNB command interpreter."""
-    
+
     def test_console_prompt(self):
         """Test the prompt attribute of the HBNB command interpreter."""
         self.assertEqual("(hbnb) ", HBNBCommand.prompt)
-        
+
     def test_function_quit(self):
         """Test the do_quit method of the HBNB command interpreter."""
         hbnb_command_instance = HBNBCommand()
         self.assertTrue(hbnb_command_instance.do_quit(None))
 
-if __name__ == '__main__':
-    unittest.main()   
+
+if __name__ == "__main__":
+    unittest.main()
