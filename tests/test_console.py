17c4375956189b972e7148fb9526a93917fc7b68
Omar-Driouch <omardriouch29@gmail.com>
2023-12-12 21:48:20 +0100

    update

--- tests/test_console.py
+++ tests/test_console.py
@@ -11,7 +11,6 @@ from models import storage
 from unittest.mock import patch
 from models.engine.file_storage import FileStorage
 from console import HBNBCommand
-from io import StringIO
 
 
 class TestHBNBCommand_prompting(unittest.TestCase):
@@ -26,14 +25,9 @@ class TestHBNBCommand_prompting(unittest.TestCase):
         hbnb_command_instance = HBNBCommand()
         self.assertTrue(hbnb_command_instance.do_quit(None))
     def test_EOF_exits(self):
-        with patch("sys.stdout", new=StringIO()) as pt:
+        with patch("sys.stdout", new=StringIO()) as output:
             self.assertTrue(HBNBCommand().onecmd("EOF"))
 
-    def test_all_single_object_dot_notation(self):
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create BaseModel"))
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("BaseModel.all()"))
 
 
 if __name__ == "__main__":
